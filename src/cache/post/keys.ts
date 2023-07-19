d502618f17e0b579df87396a88067fd987591532
ma-molinari <ma.molinari@hotmail.com>
2023-07-19 20:17:55 -0300

    chore: spaces

--- src/cache/post/keys.ts
+++ src/cache/post/keys.ts
@@ -1,9 +1,11 @@
 export const keyPostDetail = (postId: string | number) => {
   return `post:${postId}:detail`;
 };
+
 export const keyPostLikes = (postId: string | number) => {
   return `post:${postId}:likes`;
 };
+
 export const keyUserLikesPost = (userId: string | number) => {
   return `user:${userId}:post_likes`;
 };
